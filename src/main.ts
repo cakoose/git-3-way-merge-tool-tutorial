d0f7e88f004e041153f854a783227f9f740a7d86
Kannan Goundan <kannan@cakoose.com>
2024-07-30 16:44:12 -0700

    Initial

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+export const main = async (args: Array<string>): Promise<void> => {
+    if (args.length !== 2) {
+        console.log(`Expecting exactly two arguments, got ${args}.`);
+    } else {
+        const [command, value] = args;
+        switch (command) {
+            case 'add': {
+                // Make an HTTP POST request to add the value.
+                const response = await fetch('https://example.com/api/v1/stuff', {
+                    method: 'POST',
+                    body: JSON.stringify({value}),
+                });
+                if (response.status < 200 || response.status > 299) {
+                    throw new Error(`Failed to add value: ${response.statusText}.`);
+                }
+                break;
+            }
+            case 'delete': {
+                // Make an HTTP DELETE request to remove the value.
+                const response = await fetch(`https://example.com/api/v1/stuff/${encodeURIComponent(value)}`, {
+                    method: 'DELETE',
+                });
+                if (response.status < 200 || response.status > 299) {
+                    throw new Error(`Failed to delete value: ${response.statusText}.`);
+                }
+                break;
+            }
+            default: {
+                console.log(`Unknown command: ${JSON.stringify(command)}.`);
+            }
+        }
+    }
+
+    console.log('Done!');
+}
